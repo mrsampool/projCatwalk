a71fa923f20831e3388bc5a1951677d1d9ae1697
Sam Pool <sambpool@gmail.com>
2021-09-10 08:32:08 -0600

    add dummy data

---
+++ client/src/dummyData/reviewsMetadata.js
@@ -0,0 +1,31 @@
+// This is the data that should theoretically get returned from:
+//https://app-hrsei-api.herokuapp.com/api/fec2/hr-den/reviews/meta?product_id=2
+// See reviewsList.js for more explanation
+
+export const reviewsMeta = {
+  "product_id": "2",
+  "ratings": {
+    2: 1,
+    3: 1,
+    4: 2,
+    // ...
+  },
+  "recommended": {
+    0: 5
+    // ...
+  },
+  "characteristics": {
+    "Size": {
+      "id": 14,
+      "value": "4.0000"
+    },
+    "Width": {
+      "id": 15,
+      "value": "3.5000"
+    },
+    "Comfort": {
+      "id": 16,
+      "value": "4.0000"
+    },
+    // ...
+}
