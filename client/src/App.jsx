fea711dec7616329fcf92cdabe0330b0b72f0bdf
Sam Pool <sambpool@gmail.com>
2021-09-11 07:36:37 -0600

    import dummy data into App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
+// Libraries
 import React from 'react';
+
+// Sub-Components
 import { Overview } from './components/overview/overview.jsx';
 import { QAndA } from './components/qAndA/qAndA.jsx';
 import { RatingsReviews } from './components/ratingsReviews/RatingsReviews.jsx';
 import { RelatedProducts } from './components/relatedProducts/relatedProducts.jsx';
+
+//Dummy Data
+import { listQuestions, answersList } from './dummyData/anwsersList';
+import { productList, singleProduct } from './dummyData/productsList';
+import { reviewsList } from './dummyData/reviewsList';
+import { reviewsMeta } from './dummyData/reviewsMetadata';
 
 export const App = () => {
 
