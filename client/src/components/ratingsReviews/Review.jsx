7a0b7b12b940ee5d0ef89ec41a135bc1011f29b3
FaceToKeyboard <fanno.chea@gmail.com>
2021-09-13 12:26:40 -0600

    Show max 1000 characters for a review

--- client/src/components/ratingsReviews/Review.jsx
+++ client/src/components/ratingsReviews/Review.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
 export default function Review(props) {
+  let reviewBody;
+  if (props.review.body.length > 1000) {
+    reviewBody = props.review.body.slice(0, 1000);
+  } else {
+    reviewBody = props.review.body;
+  }
+
   return (
     <div id='Review' data-testid='Review' reviewid={props.review.review_id}>
       <p data-testid='reviewrating'>Rating: {props.review.rating}</p>
       <h3 data-testid='reviewsummary'>{props.review.summary}</h3>
       <h5 data-testid='reviewer_name'>{props.review.reviewer_name}</h5>
       <h5 data-testid='reviewdate'>{props.review.date}</h5>
-      <p>{props.review.body}</p>
+      <p>{reviewBody}</p>
       <p>Recommend: {props.review.recommend ? 'True' : 'False'}</p>
       <p data-testid='reviewresponse'>{props.review.response}</p>
       <p>Helpfulness: {props.review.helpfulness}</p>
