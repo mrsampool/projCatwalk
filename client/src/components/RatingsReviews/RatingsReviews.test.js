db2de5581f4b6a0a1331749930a3792c343493b6
FaceToKeyboard <fanno.chea@gmail.com>
2021-09-13 14:14:44 -0600

    test for review text being properly hidden if over 250 chars

--- client/src/components/RatingsReviews/RatingsReviews.test.js
+++ client/src/components/RatingsReviews/RatingsReviews.test.js
@@ -74,6 +74,10 @@ describe('Review component', () => {
     expect( screen.queryAllByText(/Helpfulness: 5/) ).toHaveLength(2);
   });
 
+  it('should not show beyond 250 characters in the body, initially', () => {
+    expect( screen.queryByText(/magic rings from Tiara/) ).toBeFalsy();
+  });
+
   it('should show the Show More button when review is over 250 characters', () => {
     expect( screen.queryByText(/Show More/) ).toBeTruthy();
     expect( screen.queryAllByText(/Show More/) ).toHaveLength(1);
