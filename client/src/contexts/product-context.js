5708ec8cc506b20a13ba5e0a1e9775e6e2aeb911
FaceToKeyboard <fanno.chea@gmail.com>
2021-09-14 09:10:37 -0600

    typo

--- client/src/contexts/product-context.js
+++ client/src/contexts/product-context.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { reviewsList } from '../dummyData/reviewsList.js';
 import { answersList, listQuestions } from '../dummyData/anwsersList';
-import { reviewsMeta } from '../dummyData/reviewsMetadata';;
+import { reviewsMeta } from '../dummyData/reviewsMetadata';
 
 // create ProductContext with some default values.
 // the changeProduct() function is actually implemented in App.jsx
