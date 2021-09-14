5756c0c40b235ffda4aec23d1bce4a7f2a1232a3
FaceToKeyboard <fanno.chea@gmail.com>
2021-09-14 11:40:13 -0600

    major refactoring of the global product context. No class component needed.

--- client/src/contexts/product-context.js
+++ client/src/contexts/product-context.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 import { reviewsList } from '../dummyData/reviewsList.js';
 import { answersList, listQuestions } from '../dummyData/anwsersList';
 import { reviewsMeta } from '../dummyData/reviewsMetadata';
 
-// create ProductContext with some default values.
-// the changeProduct() function is actually implemented in App.jsx
-export const ProductContext = React.createContext(
-  {
-    reviewsData: reviewsList,
-    reviewsMetadata: reviewsMeta,
-    questionsData: listQuestions,
-    answersData: answersList,
-    changeProduct: () => {}
+const defaultState = {
+  productID: 0,
+  reviewsData: reviewsList,
+  reviewsMetadata: reviewsMeta,
+  questionsData: listQuestions,
+  answersData: answersList,
+  setProductID: () => {}
+}
+
+export const ProductContext = React.createContext(defaultState);
+
+export const ProductContextProvider = (props) => {
+
+  const setProductID = (product_id) => {
+    if (product_id) {
+      axios.get('/api/example/reviews/' + product_id)
+      .then( ({data}) => setState({...state, reviewsData: data}) )
+      .then( () => axios.get('/api/example/questions'))
+      .then( ({data}) => setState({...state, questionsData: data}) )
+      .catch( err => console.log('Error from axios GET: ', err) );
+    } 
   }
-);
+
+  const initState = {
+    ...defaultState,
+    setProductID: setProductID
+  }
+
+  const [state, setState] = useState(initState);
+  
+  return (
+    <ProductContext.Provider value={state}>
+      {props.children}
+    </ProductContext.Provider>
+  );
+}
