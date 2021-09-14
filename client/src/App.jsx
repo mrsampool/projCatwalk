dadba1a32e0e6614fc169cbbbdff39ff6a970739
FaceToKeyboard <fanno.chea@gmail.com>
2021-09-14 11:47:18 -0600

    remove/cleanup unnecessary lines regarding context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,26 +12,9 @@ import { productList, singleProduct, singleProductStyles } from './dummyData/pro
 import { reviewsList } from './dummyData/reviewsList';
 import { reviewsMeta } from './dummyData/reviewsMetadata';
 
-import { ProductContextProvider, ProductContext } from './contexts/product-context.js';
+import { ProductContextProvider } from './contexts/product-context.js';
 
 export const App = () => {
-  const [reviewsData, setReviewsData] = useState(reviewsList);
-  const [questionsData, setQuestionsData] = useState(listQuestions);
-
-  const changeProduct = (product_id) => {
-    /* 
-    axios.get('/api/example/reviews/' + product_id)
-    .then(({data}) => setReviewsData(data))
-    .then(() => axios.get('/api/example/questions'))
-    .then(({data}) => setQuestionsData(data))
-    .catch(err => console.log('Error from axios GET: ', err));
-     */
-    console.log('Pass me a product ID and maybe I can get all the data for it!');
-    console.log('Current data:');
-    console.log('reviewsData: ', reviewsData);
-    console.log('questionsData: ', questionsData);
-  }
-
   return (
     <ProductContextProvider>
       <div id='App'>
