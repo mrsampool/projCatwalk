71ab75a9e900ef713cd14e3ee4231226ee07c9c8
mmp001 <gaoyf2020@gmail.com>
2021-09-14 16:16:13 -0400

    add show more button

--- client/src/components/QAndA/QuestionList/AnswerList.js
+++ client/src/components/QAndA/QuestionList/AnswerList.js
@@ -9,15 +9,20 @@ export const AnswerList = (props) =>{
   const answers = props.question.answers;
   console.log(answers);
   const answerKeys = Object.keys(answers);
-
   return (
   <div id='Answer-List' style={{color: 'blue', background: 'red'}}>
       {answerKeys.map(eachkey=>{
         const ans = answers[eachkey];
+        const [readMore, setReadMore] = useState(false);
+        const abody = ans.body;
         return (
           <div key= {ans.id}>
-            <span className="A-statement" >A: {ans.body}</span>
-            <span className="A-date" >date:{ans.date}</span>
+            <div className="A-statement" >
+              <p>A: {(readMore || abody.length < 20)? abody :`${abody.substring(0,20)}...`}
+                {(abody.length > 20) ? (<button onClick={()=>setReadMore(!readMore)}>{readMore ? 'hide content': 'show more'}</button>) : null}
+              </p>
+            </div>
+            <div className="A-date" >date:{ans.date}</div>
           </div>
         )
       })}
