a1b031aa8a0cb1b0ac2cbab80b5fcc207185ee07
Sam Pool <sambpool@gmail.com>
2021-09-16 08:15:33 -0600

    style product info and style selector

--- client/src/components/Overview/Price/Price.jsx
+++ client/src/components/Overview/Price/Price.jsx
@@ -10,14 +10,14 @@ export const Price = (props) =>{
     const {original_price, sale_price} = props.style;
 
     return(
-      <div data-testid='prod-price'>
+      <div id='prod-price' data-testid='prod-price'>
         <span className={`original-price ${sale_price ? 'struck' : ''}`} >
-          {original_price}
+          ${original_price}
         </span>
         {
           sale_price ?
             <span className='sale-price'>
-              {sale_price}
+              ${sale_price}
             </span>
             : null
         }
