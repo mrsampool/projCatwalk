f112d2ba12d7f19a404084fa0bce86f03052feb9
mmp001 <gaoyf2020@gmail.com>
2021-09-17 14:30:48 -0400

    fix big bug in search bar --no-verify

--- client/src/components/QAndA/SearchBar/SearchBar.js
+++ client/src/components/QAndA/SearchBar/SearchBar.js
@@ -5,23 +5,20 @@ import React, {useState} from 'react';
 import './SearchBar.css'
 
 export const SearchBar = (props) =>{
+
   const {changeSearchTerm} = props;
-  const [keyword, setKeyword] = useState('');
-  const SubmitHandler = (event) =>{
-    event.preventDefault();
-    changeSearchTerm(keyword);
-  }
+  const [searchTerm, setSearchTerm] = useState("");
+
+
   return (
   <div id='SearchBar' className = "search container">
-    <form onSubmit = {SubmitHandler}>
       <input
       type="text"
       name="QandASearch"
       placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..."
-      onChange = {(e)=>setKeyword(e.target.value)}
-      ></input>
-      <button type="submit">Search</button>
-    </form>
+      onChange = {(e)=>{setSearchTerm(e.target.value);changeSearchTerm(e.target.value)}}
+      />
+
   </div>
   )
 };
