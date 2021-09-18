01213aea673dabad60fbfbf9e870b3dd5a7b4cb8
FaceToKeyboard <fanno.chea@gmail.com>
2021-09-18 12:04:55 -0600

    changed name of dummy data export to be more clear/explicit when used.

--- client/src/dummyData/reviewsMetadata.js
+++ client/src/dummyData/reviewsMetadata.js
@@ -2,34 +2,7 @@
 //https://app-hrsei-api.herokuapp.com/api/fec2/hr-den/reviews/meta?product_id=2
 // See reviewsList.js for more explanation
 
-export const reviewsMeta = {
-  "product_id": "2",
-  "ratings": {
-    2: 1,
-    3: 1,
-    4: 2,
-    5: 1
-  },
-  "recommended": {
-    0: 5
-  },
-  "characteristics": {
-    "Size": {
-      "id": 14,
-      "value": "4.0000"
-    },
-    "Width": {
-      "id": 15,
-      "value": "3.5000"
-    },
-    "Comfort": {
-      "id": 16,
-      "value": "4.0000"
-    }
-  }
-}
-
-export const reviewsMetaDummy = {
+export const dummyReviewsMetadata = {
   "product_id": "2",
   "ratings": {
     2: 1,
