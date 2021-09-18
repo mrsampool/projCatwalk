95c5cacdfc2bd9939fd7a55c4454020cbec22bd5
Sam Pool <sambpool@gmail.com>
2021-09-18 08:03:21 -0600

    start related products widget

---
+++ client/src/dummyData/relatedProducts.js
@@ -0,0 +1,98 @@
+export const relatedProducts = [
+    {
+        "id": 44389,
+        "campus": "hr-den",
+        "name": "Bright Future Sunglasses",
+        "slogan": "You've got to wear shades",
+        "description": "Where you're going you might not need roads, but you definitely need some shades. Give those baby blues a rest and let the future shine bright on these timeless lenses.",
+        "category": "Accessories",
+        "default_price": "69.00",
+        "created_at": "2021-08-13T14:40:29.181Z",
+        "updated_at": "2021-08-13T14:40:29.181Z",
+        "features": [
+            {
+                "feature": "Lenses",
+                "value": "Ultrasheen"
+            },
+            {
+                "feature": "UV Protection",
+                "value": null
+            },
+            {
+                "feature": "Frames",
+                "value": "LightCompose"
+            }
+        ]
+    },
+    {
+        "id": 44390,
+        "campus": "hr-den",
+        "name": "Morning Joggers",
+        "slogan": "Make yourself a morning person",
+        "description": "Whether you're a morning person or not.  Whether you're gym bound or not.  Everyone looks good in joggers.",
+        "category": "Pants",
+        "default_price": "40.00",
+        "created_at": "2021-08-13T14:40:29.181Z",
+        "updated_at": "2021-08-13T14:40:29.181Z",
+        "features": [
+            {
+                "feature": "Fabric",
+                "value": "100% Cotton"
+            },
+            {
+                "feature": "Cut",
+                "value": "Skinny"
+            }
+        ]
+    },
+    {
+        "id": 44395,
+        "campus": "hr-den",
+        "name": "YEasy 350",
+        "slogan": "Just jumped over jumpman",
+        "description": "These stretchy knit shoes show off asymmetrical lacing and a big sculpted rubber midsole. In a nod to adidas soccer heritage.",
+        "category": "Kicks",
+        "default_price": "450.00",
+        "created_at": "2021-08-13T14:40:29.181Z",
+        "updated_at": "2021-08-13T14:40:29.181Z",
+        "features": [
+            {
+                "feature": "Sole",
+                "value": "Rubber"
+            },
+            {
+                "feature": "Material",
+                "value": "FullControlSkin"
+            },
+            {
+                "feature": "Stitching",
+                "value": "Double Stitch"
+            }
+        ]
+    },
+    {
+        "id": 44394,
+        "campus": "hr-den",
+        "name": "Blues Suede Shoes",
+        "slogan": "2019 Stanley Cup Limited Edition",
+        "description": "Touch down in the land of the Delta Blues in the middle of the pouring rain",
+        "category": "Dress Shoes",
+        "default_price": "120.00",
+        "created_at": "2021-08-13T14:40:29.181Z",
+        "updated_at": "2021-08-13T14:40:29.181Z",
+        "features": [
+            {
+                "feature": "Sole",
+                "value": "Rubber"
+            },
+            {
+                "feature": "Material",
+                "value": "FullControlSkin"
+            },
+            {
+                "feature": "Stitching",
+                "value": "Double Stitch"
+            }
+        ]
+    }
+];
