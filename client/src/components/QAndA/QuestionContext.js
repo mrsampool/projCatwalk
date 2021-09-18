387c6e0e033d0505d97b3d1339e5820ee1d8efbc
mmp001 <gaoyf2020@gmail.com>
2021-09-18 12:22:27 -0400

    fixing the add answer to continously add answer after closing the modal

--- client/src/components/QAndA/QuestionContext.js
+++ client/src/components/QAndA/QuestionContext.js
@@ -7,13 +7,31 @@ const QuestionContextProvider = (props) =>{
 
   const [questions, setQuestions] = useState(listQuestions.results) //
 
+  const [AnsId, setAnsId] = useState(300)
+
+  const storeAnsID = (currentAnsId) =>{
+    setAnsId(currentAnsId)
+    console.log(currentAnsId)
+  }
+
   const addQuestion = (question_id, question_body, question_date, asker_name, question_helpfulness, reported, answers) => {
     setQuestions([...questions, {
       question_id, question_body, question_date, asker_name,question_helpfulness, reported, answers
     }])
   }
+
+  const addAnswer = (Qid, id, body, answer_date, answerer_name, helpfulness, reported, photos) => {
+    let newAns = {id: id, body, date:answer_date, answerer_name, helpfulness, photos}
+    let targetedObj = questions.filter(obj => {
+      return obj.question_id === Qid
+    })
+    let index = questions.indexOf(targetedObj[0]);
+    targetedObj[0]['answers'][`${id}`] = newAns
+    questions[index] = targetedObj[0];
+    setQuestions([...questions])
+  }
   return (
-  <QuestionContext.Provider value={{questions, addQuestion}}>
+  <QuestionContext.Provider value={{questions, addQuestion, addAnswer, AnsId, storeAnsID}}>
     {props.children}
   </QuestionContext.Provider>
   );
